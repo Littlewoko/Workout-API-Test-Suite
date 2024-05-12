812d36f4785f6fa93e45c73d9eac839af7bb5d24
Bradley Watkins <watkinsbradley01@gmail.com>
2024-05-12 18:10:15 +0100

    created web app factory for bootstrapping api

---
+++ WorkoutWebApplicationFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Workout_API;
+
+namespace Workout_API_Test_Suite
+{
+    internal class WorkoutWebApplicationFactory : WebApplicationFactory<Program>
+    {
+    }
+}
