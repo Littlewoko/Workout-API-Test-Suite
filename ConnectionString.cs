45853cc550008edcd08d7e1f3076d2c5364a8f30
Bradley Watkins <watkinsbradley01@gmail.com>
2024-05-12 18:43:26 +0100

    connection string

---
+++ ConnectionString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_API_Test_Suite
+{
+    internal static class ConnectionString
+    {
+        internal static string GetConnectionString()
+        {
+            return "";
+        }
+    }
+}
