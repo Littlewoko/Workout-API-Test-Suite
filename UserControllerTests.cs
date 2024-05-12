7c349b71ec84a4b8af6ca14df7888e5034a2bc0f
Bradley Watkins <watkinsbradley01@gmail.com>
2024-05-12 20:14:17 +0100

    get user test

--- UserControllerTests.cs
+++ UserControllerTests.cs
@@ -90,5 +90,40 @@ namespace Workout_API_Test_Suite
                 response.StatusCode.ToString().Should().Be("BadRequest");
             }
         }
+
+        [Fact]
+        public async Task GetUser()
+        {
+            // Arrange
+            string Name = "Test";
+            string Email = "test@email.com";
+            int Id = 0;
+
+            // Arrange
+            var application = new WorkoutWebApplicationFactory();
+
+            User validUser = new User
+            {
+                Name = Name,
+                Email = Email,
+                Id = Id
+            };
+
+            var httpClient = application.CreateClient();
+
+            // Act
+            await httpClient.PostAsJsonAsync("/User", validUser);
+
+            var response = await httpClient.GetAsync($"/User?Email={Email}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var clientReponse = await response.Content.ReadFromJsonAsync<User>();
+
+            clientReponse?.Id.Should().BePositive();
+            clientReponse?.Name.Should().Be(Name);
+            clientReponse?.Email.Should().Be(Email);
+        }
     }
 }
